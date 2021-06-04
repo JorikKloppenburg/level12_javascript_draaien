d6fb1e006452fd7713dbacf0c420a9cde482722f
Jorik Kloppenburg <78646379+JorikKloppenburg@users.noreply.github.com>
2021-06-04 09:05:04 +0200

    Add files via upload

---
+++ Level12_JS_file_draaien_op_pc/script.js
@@ -0,0 +1,16 @@
+//welkomstbericht Winc studenten
+let welkom = "Hallo Winc Academy studenten";
+console.log("Welkomstbericht: ", welkom);
+
+/*
+//Dit is een grote som.
+let numbersVermenigvuldigen = 1230941 * 1823794;
+console.log(numbersVermenigvuldigen);
+// Resultaat in Terminal = 2244982810154
+
+// Bereken de uitkomst van 637263 / 54 en log de uitkomst naar de console (in dit geval jouw terminal).
+let numbersDelen = 637263 / 54;
+console.log(numbersDelen);
+// Resultaat in Terminal = 11801.166666666666
+*/
+
